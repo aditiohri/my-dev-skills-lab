9c3eb271942276d1e3dac24bfdb4b96c479adb69
Aditi Ohri <didiohri@gmail.com>
2019-10-29 18:16:18 -0500

    update form logic and UI functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-$('button').click(function() {
+$('#add').click(function() {
     var skillSet = $('#input').val();
-    $('#skills').append(`<li>${skillSet}</li>`);
+    $('#skills').append(`<li><button id="del">X</button> ${skillSet}</li>`);
 })
+
+$(document).on('click', 'li', function(){
+    $(this).remove();
+ })
+
+$('ul').css('list-style-type', 'none');
+
+
+// $('ul').addClass("HideStyle");
+// $('li').prepend(`<button class="btn-danger">X</button>`);
+
+
+// $('#list').css('text-align', 'left');
+
+// list-style-type: square
